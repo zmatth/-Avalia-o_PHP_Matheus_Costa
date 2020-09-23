5dc1ff8b1bd4c624a03ce0849ff36adbb4b98c55
Costa <matheus.costa.ol97@gmail.com>
2020-09-23 18:01:18 -0300

    essa é a tela index_teste v2

--- Teste.php
+++ Teste.php
@@ -6,11 +6,15 @@ require_once('Casamento.php');
 require_once('Nascimento.php');
 require_once('Obito.php');
 require_once('Propriedade.php');
+require_once('CompraVenda.php');
 
 $nsc1 = new Nascimento('bartolomeu','mae','nascimento','01-01-01','tabeliao1','cart1');
 $csm1 = new Casamento('barto','mae','casamento','3-3-3','tabeliao2','cart3');
 $csm2 = new Casamento('barto','mae','casamento','3-3-3','tabeliao2','cart3');
-$obito1 = new Obito(3,'Ruppert','Filho','obito','7-7-7','tabeliao8','cart4');
+$obito1 = new Obito('Ruppert','Filho','obito','7-7-7','tabeliao8','cart4');
+
+
+$compraVenda1 = new CompraVenda('Dono','2 testemunhas','Compra e venda','1-1-20','2-1-20','tabeliao6','cartorio8');
 
 
 $registroGeral = new Registro();
@@ -20,4 +24,5 @@ $registroGeral->adicionarRegistro($csm1);
 $registroGeral->adicionarRegistro($obito1);
 $registroGeral->mostrarRegistros();
 
-//tela index de teste das paginas
+
+print_r($compraVenda1);
